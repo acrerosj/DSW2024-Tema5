833d2f0adc8959a754ff4a8509e7f60e4a0978fa
Andrés Crespo <acrerosj@canariaseducacion.es>
2024-11-22 16:21:26 +0000

    access control session

---
+++ authentication/pag1.php
@@ -0,0 +1,14 @@
+<?php
+require 'control.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Página 1</h1>
+</body>
+</html>
