1e7bdda4f2453ece0b703621f62885d5b154fd63
Andrés Crespo <acrerosj@canariaseducacion.es>
2024-12-02 19:39:55 +0000

    fetch api local

--- api/employee.php
+++ api/employee.php
@@ -11,7 +11,8 @@ if (isset($_GET['id'])) {
     http_response_code(404);
   }
 } else if (isset($_GET['position'])) {
-  $employeesFilter = array_filter($employees, fn($emp) => $emp['position'] == $_GET['position']);
+  $employeesFilter = array_filter($employees, 
+    fn($emp) => $emp['position'] == $_GET['position']);
   echo json_encode(array_values($employeesFilter), JSON_PRETTY_PRINT);
 } else {
   echo json_encode($employees, JSON_PRETTY_PRINT);
--- api/index.php
+++ api/index.php
@@ -1,12 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
 </head>
+
 <body>
   <h1>Página de inicio</h1>
-  <a href="employee.php">Empleados</a>
+  <button id="btn-employees">Cargar Empleados</button>
+  <ul id="ul-employees"></ul>
+  <script>
+    const btnEmployees = document.getElementById('btn-employees');
+    const ulEmployees = document.getElementById('ul-employees');
+
+    function addEmployee(employee) {
+      const li = document.createElement('li');
+      li.textContent = employee.id + ' - ' + employee.name;
+      ulEmployees.append(li);
+    }
+
+    btnEmployees.addEventListener('click', () => {
+      let prom = fetch('employee.php')
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then(employees => {
+          console.log('Acaba la promesa!')
+          ulEmployees.innerHTML = '';
+          employees.forEach(emp => addEmployee(emp));
+        })
+        .catch(error => {
+          ulEmployees.innerHTML = '<li>Error de conexión...</li>';
+        });
+        ulEmployees.innerHTML = '<li>Cargando....</li>';
+      console.log(prom);
+    });
+  </script>
 </body>
+
 </html>
